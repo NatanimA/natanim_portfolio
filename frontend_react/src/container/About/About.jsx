105f08dbaeb2a86f6af2cfb1628ccc7c9051f118
NatanimA <Natanimabesha@gmail.com>
2023-03-14 13:56:14 +0300

    Insert Containers and components

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,41 @@
+import React,{ useState,useEffect } from 'react'
+import { motion } from 'framer-motion'
+//import { images } from '../../constants'
+import { urlForm, client } from '../../client'
+
+import './About.scss'
+
+
+
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = `*[_type == "abouts"]`;
+    client.fetch(query).then(data => {
+      setAbouts(data)
+    }).catch(err => err);
+  }, [])
+
+  return (
+    <>
+      <h2 className='head-text'>I Know that <span>Good Dev</span><br/>means <span>Good Business</span></h2>
+      <div id='about' className='app__profiles'>
+          {abouts.map((about) => (
+            <motion.div
+              whileInView={{opacity:1}}
+              whileHover={{scale:1.1}}
+              transition={{duration:0.5,type:'tween'}}
+              className="app__profile-item"
+              key={about._id}
+            >
+                <img src={urlForm(about.imgUrl)} alt={about.title}/>
+                <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
+                <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+            </motion.div>
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default About
